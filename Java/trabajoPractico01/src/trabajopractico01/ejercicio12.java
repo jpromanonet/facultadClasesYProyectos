614b244cf44727076385f173ad912dffc1fedec5
jpromanonet <programming@jpromano.net>
2019-10-13 18:32:36 -0300

    Ejercicio 12

---
+++ Java/trabajoPractico01/src/trabajopractico01/ejercicio12.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajopractico01;
+import java.util.Scanner;
+
+/**
+ *
+ * @author winEnv
+ */
+public class ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        int opcion;
+        
+        System.out.print("Introduce una frase: ");
+        cadena = sc.nextLine();
+  
+        System.out.print("Para pasar a mayuscula introduce 1, para minusculas 2: ");
+        opcion = sc.nextInt();
+        
+            if(opcion == 1){
+                System.out.println(cadena.toUpperCase());
+            } else {
+                System.out.println(cadena.toLowerCase());
+            }
+        }
+  
+}
+
